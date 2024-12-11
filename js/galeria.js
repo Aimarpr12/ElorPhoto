ab445541d748f7c292f9d87da6babe6a7863469a
in2dw3 <in2dw3@elorrieta.int>
2024-12-11 17:33:47 +0100

    añadir cropper

--- js/galeria.js
+++ js/galeria.js
@@ -4,17 +4,46 @@ $(document).ready(function () {
         return urlParams.get(param);
     }
 
-    // Obtener la imagen seleccionada de los parámetros
     const selectedImage = getQueryParam('img');
     if (selectedImage) {
-        // Cambiar la imagen principal
         $('#mainImage').attr('src', selectedImage);
     }
-    
-    $('.img-thumbnail').on('click', function () {
+
+    $('#gallery').on('click', '.img-thumbnail', function () {
         const newSrc = $(this).attr('src');
         $('#mainImage').fadeOut(300, function () {
             $(this).attr('src', newSrc).fadeIn(300);
         });
+    });
+
+    let cropper;
+
+    $('#mainImage').on('click', function () {
+        if (!cropper) {
+            cropper = new Cropper(this, {
+                aspectRatio: 1,
+                viewMode: 1
+            });
+        }
+        $('#cropButton').removeClass('d-none');
+    });
+
+    $('#cropButton').on('click', function () {
+        if (cropper) {
+            const canvas = cropper.getCroppedCanvas({
+                width: 200,
+                height: 200
+            });
+
+            const croppedImage = document.createElement('img');
+            croppedImage.src = canvas.toDataURL('image/png');
+            croppedImage.classList.add('img-thumbnail', 'thumb');
+
+            $('#gallery').append(croppedImage);
+
+            cropper.destroy();
+            cropper = null;
+            $('#cropButton').addClass('d-none');
+        }
     });
 });
